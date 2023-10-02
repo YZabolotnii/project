846b7823f20673adf1d7d67640ed90d7723f10e7
Yaroslav <yar916@ukr.net>
2023-10-02 14:23:55 +0300

    4-th commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
 "use strict";
 
-let rjkk;
+function firstTask() {
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    
+    console.log(result);
+    return result;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+firstTask();
 
 
 
